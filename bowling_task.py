c0f8cd9ff58c8212db0c99b80ec45ec3ec79a6c8
Shanky_Gupta <89583508+Shanky-07@users.noreply.github.com>
2022-05-30 15:15:02 +0530

    Add files via upload

---
+++ bowling_task.py
@@ -0,0 +1,29 @@
+def calc_bowling_score():
+  score = 0
+  for i in range(1,11):
+    shots = 2
+    shot_count = 1
+    pins_down = 0
+    while shots > 0:
+      a = int(input('number pins knocked down in {} shot of {} frame = '.format(shot_count, i)))
+      pins_down += a
+      if pins_down == 10 and shot_count == 1:
+        pins_down == 0
+        shots += 1
+        shot_count+=1
+        score+=10
+      elif pins_down == 10 and shot_count == 2:
+        pins_down = 0
+        shot_count+=1
+        shots+=1
+        score+=a
+      else:
+        score+=a
+        shot_count+=1
+      shots-=1
+  return score
+
+
+
+a = calc_bowling_score()
+print(a)
